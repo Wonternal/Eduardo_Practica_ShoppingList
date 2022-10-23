1a5a3a32caf69be7b1f0c587c5787e407a2075b1
Eduardo Camacho Díaz <fgada200@gmail.com>
2022-10-23 17:42:58 +0100

    Feature: Added some inputs and a button to save them.
    Created some inputs(text and pickers) with a useState for each one. The value is saved
    in each state. Also created a button to add them in the productList state(TO DO).

--- App.js
+++ App.js
@@ -1,12 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput} from 'react-native';
+import {useState} from "react"
+import {Picker} from '@react-native-picker/picker';
 
 const App = () => {
+
+  const [selectedQuantity, setSelectedQuantity] = useState(0)
+  
+  const [selectedType, setSelectedType] = useState("noType")
+
+  const [productName, setProductName] = useState("")
+
+  const [productList, setProductList] = useState([])
+
+
   return (
     <View style={styles.container}>
 
       <View style={styles.inputContainer}>
-        <Text>Input</Text>
+
+        <TextInput style={styles.productNameInput} placeholder=' Product name' onChangeText={(value) => setProductName(value)}/>
+
+        <Picker style={styles.quantityInput} selectedValue={selectedQuantity} onValueChange={(value) => setSelectedQuantity(value)}>
+          <Picker.Item label="Quantity" value="0" />
+          <Picker.Item label="1" value={1} />
+          <Picker.Item label="2" value={2} />
+          <Picker.Item label="3" value={3} />
+          <Picker.Item label="4" value={4} />
+          <Picker.Item label="5" value={5} />
+        </Picker>
+
+        <Picker style={styles.typeInput} selectedValue={selectedType} onValueChange={(value) => setSelectedType(value)}>
+          <Picker.Item label="Type" value="noType" />
+          <Picker.Item label="fruit" value="fruit" />
+          <Picker.Item label="vegetable" value="vegetable" />
+          <Picker.Item label="bakery" value="bakery" />
+          <Picker.Item label="fish" value="fish" />
+          <Picker.Item label="meat" value="meat" />
+        </Picker>
+
+        <View style={styles.addButton}>
+          <Button title='Add' onPress={() => console.log(productName,selectedQuantity,selectedType,)}/>
+        </View>
+
       </View>
 
       <View style={styles.listContainer}>
@@ -26,19 +61,60 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-
   },
 
   inputContainer: {
     flex: 2,
+    justifyContent: "space-evenly",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    margin: 20,
+    borderColor: "black",
+    borderWidth: 2,
+    borderRadius: 10,
+    backgroundColor: "grey"
+  },
+
+  productNameInput: {
+    borderColor: "black",
+    borderWidth: 2,
+    height: 53,
+    width: "50%",
+    marginTop: 10,
+    borderRadius: 5,
+    backgroundColor: "white"
+  },
+
+  quantityInput: {
+    borderColor: "black",
+    borderWidth: 2,
+    width: "35%",
+    marginTop: 10,
+    borderRadius: 5,
+    backgroundColor: "white"
+  },
+  
+  typeInput: {
+    borderColor: "black",
+    borderWidth: 2,
+    width: "50%",
+    marginTop: "5%",
+    borderRadius: 5,
+    backgroundColor: "white"
+  },
+
+  addButton: {
+    height: "auto",
+    width: "35%",
+    marginTop: "5%"
   },
 
   listContainer: {
-    flex: 4,
+    flex: 6,
   },
 
   buttonContainer: {
-    flex: 1,
+    flex: 2,
     justifyContent: "center",
     alignItems: "center"
   
